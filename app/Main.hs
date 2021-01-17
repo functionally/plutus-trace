62568196443287709d5d21de4f9977f604116231
Brian W Bush <code@functionally.io>
2021-01-16 20:37:50 -0700

    added printing of balance and log

--- app/Main.hs
+++ app/Main.hs
@@ -6,17 +6,23 @@ module Main where
 
 
 import Control.Monad.Freer (Eff)
-import Data.Map (Map, toList)
+import Data.Map (Map, (!), toList)
 import Game
 import Language.Plutus.Contract (Contract, ContractError)
 import Language.Plutus.Contract.Resumable (Response(Response))
 import Language.Plutus.Contract.Schema (Event)
+import Language.Plutus.Contract.Test (fundsAtAddress)
 import Language.Plutus.Contract.Trace (ContractTrace, ContractTraceEffs, ContractTraceResult(ContractTraceResult), ContractTraceState, TraceError, eventsByWallet, runTrace)
 import Data.Text.Prettyprint.Doc (pretty)
-import Wallet.Emulator (Wallet)
+import Wallet.Emulator (EmulatorState, Wallet)
+import Wallet.Emulator.MultiAgent (fundsDistribution)
 
 import qualified Language.Plutus.Contract.Trace as Trace
 import qualified Ledger.Ada                     as Ada
+import qualified Wallet.Emulator                as EM
+import qualified Wallet.Emulator.Chain          as EM
+import qualified Wallet.Emulator.MultiAgent     as EM
+import qualified Wallet.Emulator.NodeClient     as EM
 
 
 lockTrace :: ContractTrace GameSchema e () ()
@@ -53,31 +59,34 @@ guessWrongTrace =
 
 doTrace :: Contract GameSchema ContractError a
         -> Eff (ContractTraceEffs GameSchema ContractError a) ()
-        -> Either (TraceError ContractError) (ContractTraceState GameSchema (TraceError ContractError) a)
+        -> (Either (TraceError ContractError) (ContractTraceState GameSchema (TraceError ContractError) a), EmulatorState)
 doTrace contract effect =
-  case fst $ runTrace contract effect of
-    Right ((), state) -> Right state
-    Left  e           -> Left e
+  let
+    (result, emulatorState) = runTrace contract effect
+  in
+    case result of
+      Right ((), traceState) -> (Right traceState, emulatorState)
+      Left  e                -> (Left e          , emulatorState)
 
 
-traceEvents :: Contract GameSchema ContractError a
-            -> Eff (ContractTraceEffs GameSchema ContractError a) ()
-            -> Either (TraceError ContractError) (Map Wallet [Response (Event GameSchema)])
-traceEvents contract effect = eventsByWallet <$> doTrace contract effect
-
-
-printEvents :: Map Wallet [Response (Event GameSchema)] -> IO ()
-printEvents events =
-  sequence_
-    [
-      do
-        putStrLn ""
-        print wallet
-        print $ pretty responses
-    |
-      (wallet, responses) <- toList events
-    , not $ null responses
-    ]
+printEvents :: EmulatorState -> Map Wallet [Response (Event GameSchema)] -> IO ()
+printEvents state events =
+  do
+    putStrLn ""
+    print gameAddress
+    sequence_
+      [
+        do
+          putStrLn ""
+          print wallet
+          print $ pretty responses
+          print $ fundsDistribution state ! wallet
+      |
+        (wallet, responses) <- toList events
+      , not $ null responses
+      ]
+    putStrLn ""
+    print $ pretty $ EM._emulatorLog state
 
 
 main :: IO ()
@@ -87,8 +96,9 @@ main =
       do
         putStrLn ""
         putStrLn $ "----- " ++ name ++ " -----"
-        either print printEvents
-          $ traceEvents game effect
+        let
+          (traceState, emulatorState) = doTrace game effect
+        either print (printEvents emulatorState . eventsByWallet) traceState
         putStrLn ""
     |
       (name, effect) <- [
